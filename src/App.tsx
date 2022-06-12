5c9d392367fbaa8ce6590c5f0c31bb0fc530692b
AlwinBrauns <musicproductionbrauns@gmail.com>
2022-06-12 22:27:11 +0200

    web3 process.env not defined fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import { useState, useRef } from 'react';
 import Card from './Card/Card';
 import CardProperties from './Card/CardProperties';
 import {v5 as uuidv5} from 'uuid';
+import Web3 from 'web3';
 
 function App() {
   const ref = useRef<HTMLDivElement>(null)
+  const headerRef = useRef<HTMLDivElement>(null)
   
   const [selectedCard, setSelectedCard] = useState(0)
 
@@ -14,18 +16,37 @@ function App() {
 
   const [accounts, setAccounts] = useState([] as string[])
 
-  const [currentAccout, setCurrentAccount] = useState("")
+  const [currentAccount, setCurrentAccount] = useState("")
+
+  const scrollEffect = () =>{
+    if(headerRef.current) {  
+      console.log(window.scrollY)  
+      if(window.scrollY > headerRef.current.offsetTop - 100) {
+        headerRef.current.style.setProperty("--y", "-" + 100 + "px") 
+      }
+    }
+  }
 
   useEffect(() => {
+    if(headerRef.current) {
+      window.addEventListener("scroll", scrollEffect)
+    }
+    return () => {
+      window.removeEventListener("scroll", scrollEffect)
+    }
+  }, [headerRef])
+
+  const connectMetaMask = () => {
     if (typeof window.ethereum !== 'undefined') {
       window.ethereum.request({ method: 'eth_requestAccounts' }).then((_accounts: any) => {
         setAccounts(_accounts)
         setCurrentAccount(_accounts[0])
+        console.log(Web3.givenProvider)
       })
     }else {
       console.log('MetaMask is not installed!');
     }
-  }, [])
+  }
   
   useEffect(() => {
     if(cards.length <= selectedCard) {
@@ -34,7 +55,7 @@ function App() {
   }, [cards])
 
   const addCard = () => {
-    const uniqueID: string = uuidv5(Date.now().toString()+currentAccout, uuidv5.URL).toString()
+    const uniqueID: string = uuidv5(Date.now().toString()+currentAccount, uuidv5.URL).toString()
     const newCard = {
       id: uniqueID,
       text: `Card`,
@@ -61,10 +82,10 @@ function App() {
 
   return (
     <div className="App" ref={ref}>
-      <header className="App-header">
-          <span className='App-header-title'>You have selected Card Nr. <small>{cards[selectedCard]?.id ?? "-"}</small></span>
+      <header className="App-header" ref={headerRef}>
+          <span className='App-header-title'>You have selected Card Nr. <br/><small>{cards[selectedCard]?.id ?? "-"}</small></span>
           <button className='App-header-addCard accent' onClick={addCard}>Add Card</button>
-          <span>ETH-Account: {currentAccout}</span>
+          <span>{currentAccount ? `ETH-Account: ${currentAccount}`:<button className='accent' onClick={connectMetaMask}>Connect with MetaMask</button>}</span>
       </header>
       <main className="App-main">
         {
