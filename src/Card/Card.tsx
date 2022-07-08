e75546930585407fbd944258febde9fab94bc617
Alwin Brauns <alwinbrauns@Alwins-MBP.fritz.box>
2022-07-08 11:42:46 +0200

    bg color from blockchain card

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -4,11 +4,18 @@ import PulseAnimation from "../Animations/PulseAnimation"
 
 function Card ({text, onClick, onDelete, id}: CardProperties) {
     const ref = useRef<HTMLDivElement>(null)
+    const bgColor = {
+        red:  Number(id.slice(0,2)+id.slice(2,4)),
+        green: Number(id.slice(0,2)+id.slice(4,6)),
+        blue: Number(id.slice(0,2)+id.slice(6,8)),
+    }
     const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         if(ref.current) {
             PulseAnimation.start(ref.current, e.nativeEvent)
         }
-        onClick()
+        if(onClick){
+            onClick()
+        }
     }
     const handleDelete = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.stopPropagation()
@@ -22,7 +29,9 @@ function Card ({text, onClick, onDelete, id}: CardProperties) {
         }
     }
     return (
-    <div ref={ref} className={"App-main-card"} onClick={handleClick} onAnimationEnd={handleAnimationEnd}>
+    <div style={{
+        backgroundColor: `rgb(${bgColor.red}, ${bgColor.green}, ${bgColor.blue})`
+    }} ref={ref} className={"App-main-card"} onClick={handleClick} onAnimationEnd={handleAnimationEnd}>
         <span>{text} <small style={{fontSize: "0.8rem"}}>{id.substring(0, 3)}...</small></span>
         {onDelete ? <button onClick={handleDelete}>Delete</button>: null}
     </div>
