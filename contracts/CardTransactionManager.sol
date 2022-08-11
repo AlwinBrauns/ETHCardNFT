2e213d16e45b30169ea39be6e958f4b43f666022
AlwinBrauns <musicproductionbrauns@gmail.com>
2022-08-11 07:54:45 +0200

    createNewTransaction

--- contracts/CardTransactionManager.sol
+++ contracts/CardTransactionManager.sol
@@ -4,10 +4,20 @@ pragma solidity ^0.8.0;
 import './CardTransaction.sol';
 
 contract CardTransactionManager {
-    event NewTransaction(address sender, address receiver, CardTransaction transaction);
+    event NewTransaction(Card sender, Card receiver, CardTransaction transaction);
     CardTransaction[] cardTransactions;
 
     mapping(Card => uint256) cardToCountTransaction;
+
+    function createNewTransaction(Card _sender, Card _receiver, uint256 _neededWei, string memory _message) 
+    public returns(CardTransaction) {
+        CardTransaction transaction = new CardTransaction(_sender, _receiver, _neededWei, _message);
+        cardTransactions.push(transaction);
+        cardToCountTransaction[_sender]++;
+        cardToCountTransaction[_receiver]++;
+        emit NewTransaction(_sender, _receiver, transaction);
+        return transaction;
+    }
 
     function getTransactionsBySender(Card _sender) public view returns(CardTransaction[] memory _cards) {
         CardTransaction[] memory result = new CardTransaction[](cardToCountTransaction[_sender]);
